7cb94fd0e95852a0b5517c209593d4ba38cebc77
Rdk001 <rmaslenchenko95@gmail.com>
2023-04-05 16:14:30 +0000

    Deploying to gh-pages from @ Rdk001/goit-react-hw-05-movies@c5e14f8fd68f828d09692359ebd8f39cb13ed14a 🚀

---
+++ static/js/615.212c399f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[615],{615:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h2",{children:"Trending today"})})}}}]);
+//# sourceMappingURL=615.212c399f.chunk.js.map
